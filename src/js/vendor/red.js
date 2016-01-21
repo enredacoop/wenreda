e7b2b4fd952ff4ea4fc594190cb82f6acf1ccd11
pr3ssh <pablo.martin@enreda.coop>
2016-01-21 17:46:24 +0100

    Adding red.js header

---
+++ src/js/vendor/red.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+  $('body').prepend("<a href='http://enreda.coop' target='_blank' title='Red de blogs de ENREDA Cooperativa'><div style='width:100%;height:50px;background-color:#222;color:#f8f8f8;padding:0;margin:0;text-align:center;line-height:50px;'><img src='http://enreda.coop/assets/images/75x75exagono2-01.png' title='Red de blogs de ENREDA Cooperativa' alt='Red de blogs de ENREDA Cooperativa' width='65px' height='65px' /></div></a>");
+});
